504ec96e2171308f76a2add61fd931933f64c86c
ReaganOtema <reaganotema2022@gmail.com>
2025-12-05 18:34:49 +0300

    INITIAL CHANGES

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,36 +1,66 @@
+const asyncHandler = require('express-async-handler');
 const User = require('../models/usermodel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-exports.registerForm = (req, res) => {
-    res.render('users/register', { error: null });
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
-exports.register = async (req, res) => {
-    try {
-        const { username, email } = req.body;
+// @desc    Register new user
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-        if (!username || !email) {
-            return res.render('users/register', {
-                error: "All fields required."
-            });
-        }
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill all fields');
+  }
 
-        await User.create(username, email);
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
 
-        res.redirect('/users');
-    } catch (err) {
-        console.error(err);
-        res.status(500).render('users/register', {
-            error: "Error creating user"
-        });
-    }
-};
+  const user = await User.create({ name, email, password });
 
-exports.list = async (req, res) => {
-    try {
-        const [rows] = await User.getAll();
-        res.render('users/list', { users: rows });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error loading users");
-    }
-};
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// @desc    Authenticate user & get token
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+module.exports = { registerUser, loginUser };
